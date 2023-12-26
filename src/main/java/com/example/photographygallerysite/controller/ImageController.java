7708db41d093d2ff73fcb42b97a8727d5f52f5a0
bruhbot-dev <olivergeorgejames@gmail.com>
2023-12-26 18:00:00 +0000

    reformatting

--- src/main/java/com/example/photographygallerysite/controller/ImageController.java
+++ src/main/java/com/example/photographygallerysite/controller/ImageController.java
@@ -1,20 +1,16 @@
 package com.example.photographygallerysite.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
-public class MainController {
+public class ImageController {
 
-    @RequestMapping("/")
-    public String start() {
-        return "start";
+    @RequestMapping("displayImage")
+    public String firstImg(Model model, @RequestParam() String imagePath ) {
+        model.addAttribute("imagePath", imagePath);
+        return "firstImg";
     }
-
-
-
 }
