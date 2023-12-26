4d23824002a2c5853d0c6cf44c32682c414af5ee
bruhbot-dev <olivergeorgejames@gmail.com>
2023-12-26 17:45:04 +0000

    now able to save filenames to the db

--- src/main/java/com/example/photographygallerysite/PhotographyGallerySiteApplication.java
+++ src/main/java/com/example/photographygallerysite/PhotographyGallerySiteApplication.java
@@ -1,18 +1,18 @@
 package com.example.photographygallerysite;
 
 import com.example.photographygallerysite.Model.Image;
+import com.example.photographygallerysite.repo.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-
 
 
 @SpringBootApplication
 public class PhotographyGallerySiteApplication implements CommandLineRunner {
-
+    @Autowired
+    private ImageRepository repo;
 
     public static void main(String[] args) {
         SpringApplication.run(PhotographyGallerySiteApplication.class, args);
@@ -23,7 +23,7 @@ public class PhotographyGallerySiteApplication implements CommandLineRunner {
 
         Image firstImg = new Image();
         firstImg.setFilename("firstImg.jpg");
-
+        repo.save(firstImg);
     }
 }
 
