7298be4c6e43815513d4f5be0f202634a4edef12
bruhbot-dev <olivergeorgejames@gmail.com>
2023-12-26 03:25:23 +0000

    setting up application.java with commandlinerunner

--- src/main/java/com/example/photographygallerysite/PhotographyGallerySiteApplication.java
+++ src/main/java/com/example/photographygallerysite/PhotographyGallerySiteApplication.java
@@ -1,13 +1,25 @@
 package com.example.photographygallerysite;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
+
+
 @SpringBootApplication
-public class PhotographyGallerySiteApplication {
+public class PhotographyGallerySiteApplication implements CommandLineRunner {
+
 
     public static void main(String[] args) {
         SpringApplication.run(PhotographyGallerySiteApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
+
